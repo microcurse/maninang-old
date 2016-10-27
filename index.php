5170b133cd85870f6159231ecd1a9d26166fea65
Marc <marc.maninang@gmail.com>
2016-10-26 18:30:57 -0700

    PHP form changes
    Removed Materialized
    Added Bootstrap
    Added divs for form fields and fixed CSS on PHP form

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	</header>
 	<div class="main">
  		<h2>Starting off point</h2>
- 			<p>So I had all this filler "Khaled Ipsum" text in here but I figured I would actually write out the purpose of this site. I'm using a very common blog style layout for this site. It will probably change as I add more functions. I'm getting used to using scalable CSS length units such as em, rem, and vw. I really enjoy using scalable units and it feels really good to use them. I don't quite want to use Bootstrap on this site but I will most likely be playing with some sort of CSS framework. I want to add more customized functions using PHP and Javascript or jQuery. I would want to hand code these myself, so don't let me just plug and play functions!</p>
+ 			<p>So I had all this filler "Khaled Ipsum" text in here but I figured I would actually write out the purpose of this site. I'm using a very common blog style layout for this site. It will probably change as I add more functions. I'm getting used to using scalable CSS length units such as em, rem, and vw. I really enjoy scalable units and it feels really good to use them. I don't quite want to use Bootstrap on this site but I will most likely be playing with some sort of CSS framework. I want to add more customized functions using PHP and Javascript or jQuery. I would want to hand code these myself, so don't let me just plug and play functions!</p>
 			
 			<p>Here's a recent site I did for one of my clients.</p>
 			<div class="list-of-work">
@@ -17,15 +17,32 @@
 			</div>
 
 		<h3>PHP Form</h3>
-			<p>This form isn't connected to a database just yet. I'm still currently working on that right now.</p>
+			<p>This form isn't connected to a database just yet. Still a WIP.</p>
 			<div class="form-container">
-				<img src="css/images/charmander.png" alt="Charmander" width="150px">
+				<div id="form-img">
+					<img src="css/images/charmander.png" alt="Charmander" width="200px">
+				</div>
 				<form action="submit.php" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-					Name: <input type="text" name="name"><br>
-					E-mail: <input type="text" name="email"><br>
-					Who's your starting Pokemon?: <input type="text" name="pokemon"><br>
-					Explain why: <textarea name="explain" rows="4" cols="40"></textarea><br>
-					What level is it now?: <input type="number" name="level"><br>
+					<div class="form-fields">
+						<label>Name:</label> 
+						<input type="text" name="name">
+					</div>
+					<div class="form-fields">
+						<label>E-mail:</label> 
+						<input type="text" name="email">
+					</div>
+					<div class="form-fields">
+						<label>Who's your starting Pokemon?:</label> 
+						<input type="text" name="pokemon">
+					</div>
+					<div class="form-fields">
+						<label>Explain why:</label> 
+						<textarea name="explain" rows="4" cols="40"></textarea>
+					</div>
+					<div class="form-fields">
+						<label>What level is it now?:</label>
+						<input type="number" name="level">
+					</div>
 					<input type="submit">
 				</form>
 			</div>
