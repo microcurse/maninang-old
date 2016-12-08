cbf75848e3788db5c99064c2f6aac6f4f080ff32
microcurse <marc.maninang@gmail.com>
2016-12-07 16:12:01 -0800

    More contact form changes

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
-<?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$from = 'Contact Form';
-		$to = 'example@domain.com';
-		$subject = 'Message from Portfolio Contact Form ';
-
-		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
-	}
-}
-	}
-?>
 <?php include_once('templates/header.php');?>
 <body>
 	<div class="main">
@@ -65,11 +30,11 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 						<label class="form-label" for="message">Message</label>
 						<textarea name="message" rows="4" cols="40" class="form-control" placeholder="Message" tabindex="4" required></textarea>
 					</div>
+					<div class="text-center">
+						<input id="submit" name="submit" type="submit" value="Send Message" class="btn btn-start-order" action="submit.php">
+					</div>
 					<div class="form-group">
 							<?php echo $result; ?>
-					</div>
-					<div class="text-center">
-						<button type="submit" class="btn btn-start-order">Send Message</button>
 					</div>
 				</form>
 			</div>
--- submit.php
+++ submit.php
@@ -1,16 +1,36 @@
 <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST["name"];
-		$email = $_POST["email"];
-		$phone = $_POST["phone"];
-		$message= $_POST["message"];
+if (isset($_POST["submit"])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$from = 'Contact Form';
+	$to = 'example@domain.com';
+	$subject = 'Message from Portfolio Contact Form ';
+	$result = '';
 
-		$from = 'Portfolio Contact Form'
-		$to = 'example@domain.com'
-		$subject = 'Message from Portfolio Contact Form'
-
-		$body = 'From: $name\n Email: $email\n $phone\n $message';
-		$result = '<div class="alert alert-success">Thank you! I will be in touch!</div>';
+	$body ="From: $name\n E-Mail: $email\n Message:\n $message";
+	// Check if name has been entered
+	if (!$_POST['name']) {
+		$errName = 'Please enter your name';
 	}
 
+	// Check if email has been entered and is valid
+	if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errEmail = 'Please enter a valid email address';
+	}
+
+	//Check if message has been entered
+	if (!$_POST['message']) {
+		$errMessage = 'Please enter your message';
+	}
+
+// If there are no errors, send the email
+	if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+		if (mail ($to, $subject, $body, $from)) {
+			$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+		} else {
+			$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+			}
+		}
+	}
 ?>
