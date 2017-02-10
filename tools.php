a7b9675551a5de889d88457d704c4234a56cd2a1
microcurse <marc.maninang@gmail.com>
2017-02-09 22:56:30 -0800

    Calculator and ripple js added
    Finished adding calculator html and css.
    
    Added material button ripple effect to js and html.

--- tools.php
+++ tools.php
@@ -1,5 +1,35 @@
 <?php include_once('templates/header.php');?>
 <main>
 	<h2>Tools</h2>
+
+	<!-- Start Calculator -->
+		<h3>jQuery Calculator</h3>
+		<div class="calculator-container">
+			<div id="calculator">
+				<div class="top">
+					<button class="clear ripple">C</button>
+					<div class="screen"> </div>
+				</div>
+				<div class="calc-buttons">
+					<button class="ripple">7</button>
+					<button class="ripple">8</button>
+					<button class="ripple">9</button>
+					<button class="operators ripple">+</button>
+					<button class="ripple">4</button>
+					<button class="ripple">5</button>
+					<button class="ripple">6</button>
+					<button class="operators ripple">-</button>
+					<button class="ripple">3</button>
+					<button class="ripple">2</button>
+					<button class="ripple">1</button>
+					<button class="operators ripple">X</button>
+					<button class="ripple">.</button>
+					<button class="ripple">0</button>
+					<button class="equals">=</button>
+					<button class="operators ripple">÷</button>
+				</div>
+				<div style="clear:both;"></div>
+			</div>
+		</div>
 </main>
 <?php include_once('templates/footer.php');?>
